0a9dfecc5a16967e010209328b5ba5b4bac1ffa9
Osiris Román <osirisr1994@gmail.com>
2021-04-29 01:16:53 -0500

    Securing the web app using Helmet package

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
+const helmet = require("helmet");
 
 const adminRoutes = require("./routes/admin");
 const publicRoutes = require("./routes/shop");
@@ -57,6 +58,8 @@ const fileFilter = (req, file, cb) => {
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
